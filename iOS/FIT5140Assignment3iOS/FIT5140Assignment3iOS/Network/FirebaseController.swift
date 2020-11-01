e351d6d77d1821a70a06757d07a97abab4fe5661
孙楷 <704725181@qq.com>
2020-11-01 23:04:25 +1100

    update: add function for extracting speed info

--- iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Network/FirebaseController.swift
+++ iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Network/FirebaseController.swift
@@ -17,6 +17,7 @@ class FirebaseController: NSObject,DatabaseProtocol {
     var database:Firestore
     var speedLimitRef:CollectionReference?
     var normalSpeedRef:CollectionReference?
+    var speedInforList:[SpeedRecord] = []
     
     
     override init(){
@@ -31,6 +32,57 @@ class FirebaseController: NSObject,DatabaseProtocol {
             print("authResult.user.displayName")
             self.speedLimitRef = self.database.collection("speedLimitRecord")
             self.normalSpeedRef = self.database.collection("normalSpeedRecord")
+            self.setUpListeners()
+        })
+    }
+    
+    func setUpListeners(){
+        self.speedLimitRef?.addSnapshotListener({(querySnapshot,error) in
+            guard let querySnapshot = querySnapshot else{
+                print("Error fetching documents")
+                return
+            }
+            self.parseSpeedLimitSnapshot(snapshot:querySnapshot)
+        })
+    }
+    
+    func parseSpeedLimitSnapshot(snapshot:QuerySnapshot){
+        snapshot.documentChanges.forEach({(change) in
+            let speedRecordId = change.document.documentID
+            print(speedRecordId)
+            var parsedSpeedDocument:SpeedRecord?
+            
+            do{
+                parsedSpeedDocument = try change.document.data(as: SpeedRecord.self)
+            }catch{
+                print("Unable to decode the speed record")
+                return
+            }
+            
+            guard let speedRecord = parsedSpeedDocument else{
+                print("Document does not exist")
+                return
+            }
+            
+            speedRecord.id = speedRecordId
+            switch change.type{
+            case .added:
+                speedInforList.append(speedRecord)
+            case .modified:
+                if let index = getSpeedRecordById(id: speedRecordId) {
+                    speedInforList[index] = speedRecord
+                }
+            case .removed:
+                if let index = getSpeedRecordById(id: speedRecordId){
+                    speedInforList[index] = speedRecord
+                }
+            }
+        })
+    }
+    
+    func getSpeedRecordById(id:String)->Int?{
+        return speedInforList.firstIndex(where: {(speed) in
+            speed.id == id
         })
     }
     
