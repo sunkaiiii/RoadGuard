e2c8ee9e4c00da1ec435772d31fdea0f4332315f
jiyang_xu <jxuu0077@student.monash.edu>
2020-11-17 00:03:50 +1100

    update: adding horizontal barchart table view

--- iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Views/RecordDetailCellViews/RecordDetailChartCell.swift
+++ iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Views/RecordDetailCellViews/RecordDetailChartCell.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 import Charts
-class RecordDetailChartCell: UITableViewCell, ChartViewDelegate {
+class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDelegate, UITableViewDataSource {
+
 
     @IBOutlet weak var backgroundVisualEffectView: UIVisualEffectView!
     
     @IBOutlet weak var lineChartOutlet: LineChartView!
 
-    @IBOutlet weak var horizontalBarChartOutlet: HorizontalBarChartView!
+    @IBOutlet weak var barChartTableVIew: UITableView!
+    
+    let CELL_ID = BarChartTableViewCell.identifier
+    //需要改为传入的值
+    var barChartTableViewDataSource = [("street1", 60),("street2", 80),("street3", 90),("street4", 60),("street5", 120),("street6", 180),("street7", 260),("street1", 360),("street1", 120),("street1", 40),("street1", 20)]
 
     static let identifier = "RecordDetailChartCell"
     static func nib()->UINib{
@@ -23,6 +28,10 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate {
         super.awakeFromNib()
         backgroundVisualEffectView.layer.cornerRadius = 24
         backgroundVisualEffectView.contentView.layer.cornerRadius = 24
+        barChartTableVIew.delegate = self
+        barChartTableVIew.dataSource = self
+        barChartTableVIew.register(BarChartTableViewCell.nib(), forCellReuseIdentifier: CELL_ID)
+
 
         lineChartOutlet.noDataText = "No Data Avaiable"
         lineChartOutlet.backgroundColor = .clear
@@ -41,16 +50,7 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate {
 
         lineChartOutlet.legend.enabled = false
     
-        horizontalBarChartOutlet.noDataText = "No Data Avaiable"
-        horizontalBarChartOutlet.backgroundColor = .clear
-        horizontalBarChartOutlet.drawGridBackgroundEnabled = false
-        horizontalBarChartOutlet.rightAxis.enabled = false
-        horizontalBarChartOutlet.leftAxis.enabled = false
-        horizontalBarChartOutlet.xAxis.enabled = false
-        horizontalBarChartOutlet.legend.enabled = false
 
-        horizontalBarChartOutlet.drawValueAboveBarEnabled = true
-        horizontalBarChartOutlet.fitBars = true
         // Initialization code
     }
 
@@ -59,6 +59,43 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate {
 
         // Configure the view for the selected state
     }
-    
+
+    // MARK: - TableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return barChartTableViewDataSource.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath) as! BarChartTableViewCell
+        //给cell传值
+        cell.backgroundVisualEffect.layer.cornerRadius = 24
+        cell.backgroundVisualEffect.contentView.layer.cornerRadius = 24
+
+        let maxPossibleSpeed = CGFloat(360)
+        let speed = CGFloat(barChartTableViewDataSource[indexPath.row].1)
+        let name = barChartTableViewDataSource[indexPath.row].0
+        let widthRatio = speed/maxPossibleSpeed
+//        cell.backgroundVisualEffect.frame.size.width = (self.frame.width - 50.0) * widthRatio
+//        cell.backgroundVisualEffect.contentView.frame.size.width = (self.frame.width - 50.0) * widthRatio
+        cell.barWidthConstraint.constant = (self.frame.width - 50.0) * widthRatio
+        cell.streetNameLabel.text = name
+        cell.speedLabel.text =  "\(speed)km/h"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.barChartTableVIew.frame.height / 5
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    func tableView(_ tableView: UITableView,didSelectRowAt indexPath: IndexPath) {       
+        tableView.deselectRow(at:indexPath,animated:true)
+    }
 }
 
