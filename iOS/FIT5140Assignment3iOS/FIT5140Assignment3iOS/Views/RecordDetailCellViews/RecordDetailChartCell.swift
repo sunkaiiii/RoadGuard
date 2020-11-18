d78067b89ab5dfc34d181e5082520831023ad6ad
孙楷 <704725181@qq.com>
2020-11-19 01:34:05 +1100

    update: show speed chart inforamtion

--- iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Views/RecordDetailCellViews/RecordDetailChartCell.swift
+++ iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/Views/RecordDetailCellViews/RecordDetailChartCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Charts
-class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDelegate, UITableViewDataSource {
+class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDelegate, UITableViewDataSource,DefaultHttpRequestAction {
 
 
     @IBOutlet weak var backgroundVisualEffectView: UIVisualEffectView!
@@ -17,8 +17,8 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDele
     @IBOutlet weak var barChartTableVIew: UITableView!
     
     let CELL_ID = BarChartTableViewCell.identifier
-    //需要改为传入的值
-    var barChartTableViewDataSource = [("street1", 60),("street2", 80),("street3", 90),("street4", 60),("street5", 120),("street6", 180),("street7", 260),("street1", 360),("street1", 120),("street1", 40),("street1", 20)]
+    var barChartTableViewDataSource:[(String,Int)] = []
+    var speedList:[SpeedRecord] = []
 
     static let identifier = "RecordDetailChartCell"
     static func nib()->UINib{
@@ -53,6 +53,81 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDele
 
         // Initialization code
     }
+    
+    func initLineChart(_ drivingRecord:DrivingRecordResponse?){
+        guard let drivingRecord = drivingRecord else{
+            return
+        }
+        //configure line chart data
+        let dataSet = LineChartDataSet(entries: getLineChartEntries(drivingRecord), label: "km/h")
+        //disable the big circle in the chart
+        dataSet.drawCirclesEnabled = false
+        //make the line looks more smooth
+        dataSet.mode = .cubicBezier
+        //line width
+//            dataSet.lineWidth = 1
+        //white line
+        dataSet.setColor(.white)
+        let lineChartData = LineChartData(dataSet: dataSet)
+        //hide the number on each point of the line
+        lineChartData.setDrawValues(false)
+
+        self.lineChartOutlet?.data = lineChartData
+
+    }
+    
+    private func getLineChartEntries(_ drivingRecord:DrivingRecordResponse)->[ChartDataEntry]{
+        var lineChartDataEntries : [ChartDataEntry] = []
+        if let appdelegate = UIApplication.shared.delegate as? AppDelegate, let databaseController = appdelegate.firebaseController, let id = drivingRecord.id{
+            let speedList = databaseController.getSpeedRecordByRecordId(id)
+            var index = 0.0
+            speedList.forEach({(speed) in
+                lineChartDataEntries.append(ChartDataEntry(x: index, y: Double(speed.currentSpeed)))
+                index += 1
+            })
+        }
+        
+        return lineChartDataEntries
+    }
+    
+    func initBarChart(_ drivingRecord:DrivingRecordResponse?){
+        guard let drivingRecord = drivingRecord else{
+            return
+        }
+        if let appdelegate = UIApplication.shared.delegate as? AppDelegate, let databaseController = appdelegate.firebaseController, let id = drivingRecord.id{
+            var lats:[Double] = []
+            var longs:[Double] = []
+            let speedList = databaseController.getSpeedRecordByRecordId(id)
+            self.speedList = speedList
+            speedList.forEach({(speed) in
+                lats.append(speed.latitude)
+                longs.append(speed.longitude)
+            })
+            requestCachegableDataFromRestfulService(api: GoogleApi.nearestRoads, model: NearestRoadsRequest(lat: lats, log: longs), jsonType: NearestRoadResponse.self, cachegableHelper: NearestRoadResponseCacheHelper())
+        }
+    }
+
+    func handleResponseDataFromRestfulRequest(helper: RequestHelper, url: URLComponents, accessibleData: AccessibleNetworkData) {
+        switch helper.restfulAPI as? GoogleApi {
+        case .nearestRoads:
+            let roads:NearestRoadResponse = accessibleData.retriveData()
+            if roads.snappedPoints.count == 0{
+                return
+            }
+            var currentIndex = roads.snappedPoints[0].originalIndex
+            barChartTableViewDataSource.append((roads.snappedPoints[0].placeID,speedList[currentIndex].currentSpeed))
+            roads.snappedPoints.forEach({(point) in
+                if currentIndex != point.originalIndex{
+                    currentIndex = point.originalIndex
+                    barChartTableViewDataSource.append((point.placeID,speedList[currentIndex].currentSpeed))
+                }
+                
+            })
+            barChartTableVIew.reloadData()
+        default:
+            return
+        }
+    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -71,28 +146,8 @@ class RecordDetailChartCell: UITableViewCell, ChartViewDelegate, UITableViewDele
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath) as! BarChartTableViewCell
-        //给cell传值
-        cell.backgroundVisualEffect.layer.cornerRadius = 24
-        cell.backgroundVisualEffect.contentView.layer.cornerRadius = 24
-
-        let maxPossibleSpeed = CGFloat(360)
-        let speed = CGFloat(barChartTableViewDataSource[indexPath.row].1)
-        let name = barChartTableViewDataSource[indexPath.row].0
-        let widthRatio = speed/maxPossibleSpeed
-
-        let baseWidth = CGFloat(130)
-        let totalGapWidth = CGFloat(110)
-
-        if speed == CGFloat(0) {
-            cell.barWidthConstraint.constant = baseWidth
-        } else {
-            cell.barWidthConstraint.constant = baseWidth + (self.frame.width - baseWidth - totalGapWidth) * widthRatio
-        }
-
-
-        cell.streetNameLabel.text = name
-        //we don't use CGFloat result here
-        cell.speedLabel.text =  "\(barChartTableViewDataSource[indexPath.row].1)km/h"
+        let data = barChartTableViewDataSource[indexPath.row]
+        cell.initBarChartCell(data)
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
