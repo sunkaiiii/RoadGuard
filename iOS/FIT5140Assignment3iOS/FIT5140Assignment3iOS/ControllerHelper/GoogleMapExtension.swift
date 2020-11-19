f67a47fc07546e4787aae9dd8a9ce5a5c12a2b3c
孙楷 <704725181@qq.com>
2020-11-19 23:00:44 +1100

    update: dark mode for google map view

---
+++ iOS/FIT5140Assignment3iOS/FIT5140Assignment3iOS/ControllerHelper/GoogleMapExtension.swift
@@ -0,0 +1,50 @@
+//
+//  CustomGoogleMapView.swift
+//  FIT5140Assignment3iOS
+//
+//  Created by sunkai on 19/11/20.
+//
+
+import Foundation
+import GoogleMaps
+
+// dark mode style, referencces on https://developers.google.com/maps/documentation/ios-sdk/styling
+extension GMSMapView{
+    func changeToDarkMode(){
+        do {
+              // Set the map style by passing the URL of the local file.
+              if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                self.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+              } else {
+                NSLog("Unable to find style.json")
+              }
+            } catch {
+              NSLog("One or more of the map styles failed to load. \(error)")
+            }
+    }
+    
+    func changeToLightMode(){
+        self.mapStyle = nil
+    }
+    
+    open override func didMoveToSuperview() {
+        handleWithTheme()
+    }
+    
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if #available(iOS 13.0, *){
+            // sense theme swtich
+            if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection){
+                handleWithTheme()
+            }
+        }
+    }
+    
+    private func handleWithTheme(){
+        if traitCollection.userInterfaceStyle == .dark{
+            changeToDarkMode()
+        }else{
+            changeToLightMode()
+        }
+    }
+}
